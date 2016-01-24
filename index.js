b8fab1efea740cbd7f606e019d659bf04ae33c34
srph <seraphipod@gmail.com>
2016-01-24 08:43:14 +0800

    Init commit./

---
+++ index.js
@@ -0,0 +1,20 @@
+var axios = require('axios');
+var join = require('url-join');
+var isAbsoluteURL = require('is-absolute-url');
+
+function base(url) {
+  var undo = axios.interceptors.request.use(function(config) {
+    if ( !isAbsoluteURL(config.url) ) {
+      config.url = join(url, config.url);
+    }
+
+    return config;
+  }, function (error) {
+    // Do something with request error
+    return Promise.reject(error);
+  });
+
+  return undo;
+};
+
+module.exports = base;
